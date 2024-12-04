74151695be59bd6e439a7472af55586307ad4bf7
Jordan-Doctolero <jordandoctolero2025@gmail.com>
2024-12-04 10:16:40 -1000

    Update getMonthComments.java

--- getMonthComments.java
+++ getMonthComments.java
@@ -1,7 +1,7 @@
 /*
  * This locates the month that you want to find with a number you pass.
  * This methods finds the month at the index of the String that is passed as the parameter n.
- * It uses logic statements and loops to find the month from a single string containing all the twleve months in a year.
+ * It uses logic statements and loops to find the month from a single string containing all the twelve months in a year.
 */
 public static String getMonth(int n)
 	{
