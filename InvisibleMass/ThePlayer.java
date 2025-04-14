52df98d0411055660cbd5dfc5953f1039cd3bfab
Jordan-Doctolero <jordandoctolero2025@gmail.com>
2025-04-13 18:27:30 -1000

    Update ThePlayer.java

--- InvisibleMass/ThePlayer.java
+++ InvisibleMass/ThePlayer.java
@@ -5,75 +5,75 @@ public class ThePlayer {
 	
 	//Data Attributes
 	
-	   /**
-		* This creates private data. Basically the numbers we will pass it.
-		* It is also what the class needs in order to function.
-		*/
+	/**
+  	 * This creates private data. Basically the numbers we will pass it.
+ 	 * It is also what the class needs in order to function.
+	 */
 		
-		private int rPos;
-		//This creates the X position
-		private int cPos;
-		//This creates the Y position
+	private int rPos;
+	//This creates the X position
+	private int cPos;
+	//This creates the Y position
 		
-		//Constructor
+	//Constructor
 		
-		/**
-		 * This is where I make the characters itself.
-		 */
+	/**
+         * This is where I make the characters itself.
+	 */
 		
-		/*
-		 * This is making a Character constructor.
-		 * It has all the variables we have data attributes for above.
-		 */
-		public ThePlayer()
-		{
-			rPos=0;
-			//This sets their x position to 0.
-			cPos=0;
-			//This sets their y position to 0.
-		}
+	/*
+	 * This is making a Character constructor.
+         * It has all the variables we have data attributes for above.
+	 */
+	public ThePlayer()
+	{
+		rPos=0;
+		//This sets their x position to 0.
+		cPos=0;
+		//This sets their y position to 0.
+	}
 		
-		//Functionality's
+	//Functionality's
 		
-		/**
-		 * This is what the class is going to do when I call it in the main method.
-		 * These are setters and getters to run the nature of the game.
-		 * They take things from the data attributes and constructors.
-		 */
+	/**
+	 * This is what the class is going to do when I call it in the main method.
+  	 * These are setters and getters to run the nature of the game.
+	 * They take things from the data attributes and constructors.
+	 */
 		
-		/*
-		 * This gets the position of the players row.
-		 */
-		public int getRPos()
-		{
-			return(rPos);
-			//This returns the said position.
-		}
+	/*
+	 * This gets the position of the players row.
+	 */
+	public int getRPos()
+	{
+		return(rPos);
+		//This returns the said position.
+	}
 		
-		/*
-		 * This sets the position of the players row.
-		 */
-		public void setRPos(int rPos)
-		{
-			this.rPos=rPos;
-			//This is setting the position of the Character to the int passed.
-		}
+	/*
+	 * This sets the position of the players row.
+	 */
+	public void setRPos(int rPos)
+	{
+		this.rPos=rPos;
+		//This is setting the position of the Character to the int passed.
+	}
 		
-		/*
-		 * This gets the position of the players column.
-		 */
-		public int getCPos()
-		{
-			return(cPos);
-			//This returns the said position.
-		}
+	/*
+	 * This gets the position of the players column.
+	 */
+	public int getCPos()
+	{
+		return(cPos);
+		//This returns the said position.
+	}
 		
-		/*
-		 * This sets the position of the players column.
-		 */
-		public void setCPos(int cPos)
-		{
-			this.cPos=cPos;
-			//This is setting the position of the Character to the int passed.
-		}	
+	/*
+	 * This sets the position of the players column.
+	 */
+	public void setCPos(int cPos)
+	{
+		this.cPos=cPos;
+		//This is setting the position of the Character to the int passed.
+	}	
 }
