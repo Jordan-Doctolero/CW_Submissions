54967e6cda9f6a3cbb512cd4f5076cc09d98ff40
Jordan-Doctolero <jordandoctolero2025@gmail.com>
2025-05-06 13:44:03 -1000

    Update PuppyDog.java

--- Inheritance/PuppyDog.java
+++ Inheritance/PuppyDog.java
@@ -36,8 +36,8 @@ public class PuppyDog extends Dog{
 		
 	/**
 	 * This is what the class is going to do when I call it in the main method.
-     * These are setters and getters to run the nature of the game.    
-     * They take things from the data attributes and constructors.
+         * These are setters and getters to run the nature of the game.    
+         * They take things from the data attributes and constructors.
 	 */
 
 	/*
